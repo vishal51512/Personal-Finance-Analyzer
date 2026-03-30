e0ec58bf3bb9b6416b87993c27791414f2f5d035
Vishal songara <vishalsongara511@gmail.com>
2026-03-30 06:37:20 +0530

    fix    backend

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
-from app.routes import upload
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import upload, analytics
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(upload.router)
 app.include_router(analytics.router)
